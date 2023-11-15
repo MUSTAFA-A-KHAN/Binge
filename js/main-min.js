c39a8748dde32e828a5cb62c31c9dabeb394adc7
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2023-11-15 17:50:03 +0530

    Update main-min.js

--- js/main-min.js
+++ js/main-min.js
@@ -5,7 +5,7 @@ function getMovies(t) {
       let e = t.data.Search,
         i = "";
       $.each(e, (t, e) => {
-        i += `\n                <div class="col-md-3">\n                <div class="well text-center">\n                <img src="${e.Poster}">\n                <h5>${e.Title}</h5>\n                <a onclick="movieSelected('${e.imdbID}')" class ="btn btn-danger" href="#">Movie Detailss</a>\n    
+        i += `\n                <div class="col-md-3">\n                <div class="well text-center">\n                <img src="${e.Poster}">\n                <h5>${e.Title}</h5>\n                <a onclick="movieSelected('${e.imdbID}')" class ="btn btn-danger" href="#">Movie Details</a>\n    
        
         
         <button class="btn btn-danger" onclick="openMovieWin1('${e.Type}', '${e.imdbID}')">▶Play</button>
